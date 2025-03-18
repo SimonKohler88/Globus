5eb11d02eadaf472b87be065bf30fa90303c7782
SimonKohler88 <skohler88@gmail.com>
2025-03-18 19:06:28 +0100

    before changing avMMprotocol

---
+++ FPGA/VHDL/my_sys_components/scripts/raw_data_to_addr_and_data.py
@@ -0,0 +1,23 @@
+import os
+
+if __name__ == '__main__':
+    file = 'Earth_relief_120x256_raw2.txt'
+
+    file_name, ext = os.path.splitext(file)
+    addr_offset = 0x00
+
+    with open(file, 'r') as f:
+        all_lines = f.readlines()
+
+    out_string = ''
+    for i, each_line in enumerate(all_lines):
+        row_nr = addr_offset + i * 2
+        data_1 = each_line[:4]
+        data_2 = each_line[4:].strip() + '00'
+
+        out_string += f'{row_nr:08x}  {data_1}\n{row_nr + 1:08x}  {data_2}\n'
+
+    new_name = f'{file_name}_addr_space{ext}'
+
+    with open(new_name, 'w') as f:
+        f.write(out_string)
