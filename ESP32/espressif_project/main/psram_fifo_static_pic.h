dbae9c4e67f015a0226c73b772f70d2014a9b6f5
SimonKohler88 <skohler88@gmail.com>
2025-04-04 21:43:22 +0200

    fix test pic

---
+++ ESP32/espressif_project/main/psram_fifo_static_pic.h
@@ -0,0 +1,17 @@
+//
+// Created by skohl on 04.04.2025.
+//
+
+#ifndef PSRAM_FIFO_STATIC_PIC_H
+#define PSRAM_FIFO_STATIC_PIC_H
+
+
+
+#include "stdint.h"
+
+void ext_copy_static_pic_to_PSRAM( uint8_t* start_ptr );
+
+
+
+
+#endif //PSRAM_FIFO_STATIC_PIC_H
--- FPGA/VHDL/my_sys_components/scripts/generate_c_array_from_raw.py
+++ FPGA/VHDL/my_sys_components/scripts/generate_c_array_from_raw.py
@@ -1,8 +1,68 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+
+def get_string_1(c_arr):
+    s = """
+    #include "psram_fifo_static_pic.h"
+    #include <string.h>
+    #include "freertos/FreeRTOS.h"
+    #include "freertos/task.h"
+
+    void ext_copy_static_pic_to_PSRAM( uint8_t* start_ptr )
+    {
+        /* Copy from code directly to PSRAM, So it does not use Stack */
+
+        uint8_t* temp_ptr = start_ptr;
+        temp_ptr = start_ptr;
+        memcpy( temp_ptr, ( uint8_t[] ) {
+        """
+    bytes_per_block = int(len(c_arr) / 160)
+    block = 0
+    block_byte = 0
+    for i, each in enumerate(c_arr):
+        if block_byte % 20 == 0 and block_byte != 0:
+            s += '\n\t'
+
+        if block_byte == bytes_per_block:
+            s += f' }},{bytes_per_block} );\n\n'
+            # s+= '\tvTaskDelayUntil( 1 );\n\n'
+            s += '\ttaskYIELD();\n\n'
+            s += f'\ttemp_ptr += {bytes_per_block};\n'
+            s += '\tmemcpy( temp_ptr, ( uint8_t[] ) {\n'
+            s += '\t'
+            block_byte = 0
+        s += each
+        s += ', '
+        block_byte += 1
+
+    s += f' }},{bytes_per_block} );\n'
+    s += '}'
+    return s
+
+
+def get_string_2(c_arr):
+    s = """
+    #include "psram_fifo_static_pic.h"
+    #include <string.h>
+    #include "freertos/FreeRTOS.h"
+    #include "freertos/task.h"
+
+    void ext_copy_static_pic_to_PSRAM( uint8_t* start_ptr )
+    {
+        /* Copy from code directly to PSRAM, So it does not use Stack */
+
+        
+        """
+    for i, each in enumerate(c_arr):
+        s += f'\tstart_ptr[{i}] = {each};\n'
+    s += '}'
+    return s
+
 if __name__ == '__main__':
     original_data_file = 'Earth_relief_120x256_raw2.txt'
+
+    c_file_name_2_write = 'psram_fifo_static_pic.c'
     with open(original_data_file, 'r') as f:
         orig_data = f.readlines()
 
@@ -10,14 +70,13 @@ if __name__ == '__main__':
 
     c_arr = []
 
-
     for each in orig_data:
         c_arr.append(f'0x{each[:2]}')
         c_arr.append(f'0x{each[2:4]}')
         c_arr.append(f'0x{each[4:]}')
 
-    for i, each in enumerate(c_arr):
-        if i%20 == 0 and i != 0:
-            print('\t')
-        print(each, end= ', ')
-    print(i)
+    s = get_string_2(c_arr)
+
+    with open(c_file_name_2_write, 'w') as f:
+        f.write(s)
+    print()
