d64dd5d35128e1bb6274d129131f604ea393726c
SimonKohler88 <skohler88@gmail.com>
2025-04-20 00:12:01 +0200

    few different colored worlds

--- PythonServer/gif_maker/make_gif_from_np_array.py
+++ PythonServer/gif_maker/make_gif_from_np_array.py
@@ -7,10 +7,11 @@ import time
 
 if __name__ == '__main__':
     arr_file = 'pic_raw.npy'
+    arr_file = 'pic_world_blue_marble_nov2004.npy'
 
     pics = np.load(arr_file)
 
-    with imageio.get_writer('globe.gif', mode='I', fps=20) as writer:
+    with imageio.get_writer('globe_world_blue_marble_nov2004.gif', mode='I', loop=0, fps=20) as writer:
 
         for each in pics:
             # image = imageio.imread(filename)
